4518be555015e23cdbf8ff798e889e9220e354ac
Sentret <j.andrianov@gmail.com>
2017-10-15 22:13:43 +0300

    Изменил структуру

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MarksTable from './MarksTable';
-import StudentInfoComponent from './StudentInfoComponent';
+import MarksTable from './StudentView/MarksTable';
+import StudentInfoComponent from './StudentView/StudentInfoComponent';
 
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
