3fcb56c63d321b5fa2df57cff2d8fe11d5c10e4d
Sentret <j.andrianov@gmail.com>
2018-02-10 19:48:01 +0300

    Post

--- src/Auth.js
+++ src/Auth.js
@@ -3,6 +3,39 @@ import { Link, Switch, Route } from 'react-router-dom'
 
 
 class Login extends Component {
+ constructor() {
+    super();
+     this.state = {
+      username: null,
+      password: null
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.click = this.click.bind(this);
+  }
+    
+  
+
+
+  click(event) {
+    fetch('https://mywebsite.com/endpoint/', {
+       method: 'POST',
+      headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    username: this.state.username,
+    password: this.state.password,
+  })
+})
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value
+    });
+  }
 
 
   render() {
@@ -13,15 +46,15 @@ class Login extends Component {
         <form class="form-signin">
         <h3 class="form-signin-heading">Аутентификация</h3>
         <label for="inputEmail" class="sr-only">Имя пользователя</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Имя пользователя" required="" autofocus=""/>
+        <input name = 'username' type="email" id="inputEmail" class="form-control" placeholder="Имя пользователя" required=""  onChange={this.handleChange} autofocus=""/>
         <label for="inputPassword" class="sr-only">Пароль</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required=""/>
+        <input name = 'password'  type="password" id="inputPassword" class="form-control" placeholder="Пароль" onChange={this.handleChange} required=""/>
         <div class="checkbox">
           <label>
             <input type="checkbox" value="remember-me"/> Оставаться в системе
           </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" onClick={this.click} >Войти</button>
       </form>
         </div>
     )
